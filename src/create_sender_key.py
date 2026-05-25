7088c9cf7066b136286ed58c5009ff7244d35bb4
ParmidaAzhir <140430728+ParmidaAzhir@users.noreply.github.com>
2026-05-25 18:35:19 +0200

    create_sender_key.py

--- src/create_sender_key.py
+++ src/create_sender_key.py
@@ -1,19 +1,19 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa 
 from cryptography.hazmat.primitives import serialization
 
-# generate private key
-private_key = rsa.generate_private_key(   #This line creates a new private key using the RSA algorithm.
-    public_exponent=65537,
+# generate rsa private key
+private_key = rsa.generate_private_key(  
+    public_exponent=65537, #mathematical value required by the RSA algorithm
     key_size=2048
     #Key size defines the strength of the key. 2048 bits is a secure standard.
 )
 
 # save
-with open("keys/sender_key.pem", "wb") as f:
-    f.write(
-        private_key.private_bytes(
+with open("keys/sender_key.pem", "wb") as f: #Open sender_key.pem
+    f.write( # Write the private key bytes into the PEM file.
+        private_key.private_bytes( 
             encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            format=serialization.PrivateFormat.TraditionalOpenSSL, #defines the private key format as Traditional OpenSSL
             encryption_algorithm=serialization.NoEncryption()
         )
     )
