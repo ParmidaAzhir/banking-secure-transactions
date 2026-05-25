b49ff6d1121c57ec5c806f23a109298093adae08
ParmidaAzhir <140430728+ParmidaAzhir@users.noreply.github.com>
2026-05-25 18:34:25 +0200

    create_fake_sender_key.py

--- src/create_fake_sender_key.py
+++ src/create_fake_sender_key.py
@@ -1,14 +1,14 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-fake_key = rsa.generate_private_key(
+fake_key = rsa.generate_private_key(  #Generate fake sender RSA public/private key pair.
     public_exponent=65537,
     key_size=2048
 )
 
 with open("keys/fake_sender_key.pem", "wb") as f:
     f.write(
-        fake_key.private_bytes(
+        fake_key.private_bytes(##Convert fake sender private key into PEM bytes.
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.NoEncryption()
