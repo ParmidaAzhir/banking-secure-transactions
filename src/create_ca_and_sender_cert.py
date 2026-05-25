ba137af0b79d7cb6d5f6007567fd83a3fa62c3f4
ParmidaAzhir <140430728+ParmidaAzhir@users.noreply.github.com>
2026-05-25 18:28:43 +0200

    create_ca_and_sender_cert.py

--- src/create_ca_and_sender_cert.py
+++ src/create_ca_and_sender_cert.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.x509.oid import NameOID #defines standard certificate identity fields
+from cryptography.hazmat.primitives import hashes, serialization #save/load pem files
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 def save_private_key(path, key):
@@ -34,18 +34,18 @@ def main():
     now = datetime.now(timezone.utc)
 
     ca_cert = (
-        x509.CertificateBuilder()
-        .subject_name(ca_subject)
-        .issuer_name(ca_subject)
-        .public_key(ca_key.public_key())
+        x509.CertificateBuilder() 
+        .subject_name(ca_subject) #owner = ca
+        .issuer_name(ca_subject) #issuer (self)
+        .public_key(ca_key.public_key()) #Adds CA public key.
         .serial_number(x509.random_serial_number())
         .not_valid_before(now)
         .not_valid_after(now + timedelta(days=3650))
-        .add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True)
-        .sign(ca_key, hashes.SHA256())
+        .add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True) #Marks this certificate as a CA certificate.
+        .sign(ca_key, hashes.SHA256()) #Signs CA certificate using CA private key (self) and sha256
     )
 
-    # Load sender private key
+   # Open the sender private key file and load the PEM private key.
     with open("keys/sender_key.pem", "rb") as f:
         sender_key = serialization.load_pem_private_key(f.read(), password=None)
 
@@ -59,11 +59,11 @@ def main():
         .serial_number(x509.random_serial_number())
         .not_valid_before(now)
         .not_valid_after(now + timedelta(days=3650))
-        .add_extension(x509.BasicConstraints(ca=False, path_length=None), critical=True)
-        .sign(ca_key, hashes.SHA256())
+        .add_extension(x509.BasicConstraints(ca=False, path_length=None), critical=True) # not ca
+        .sign(ca_key, hashes.SHA256()) #Signs the sender certificate using the CA private key and SHA256.
     )
 
-    save_private_key("keys/ca_key.pem", ca_key)
+    save_private_key("keys/ca_key.pem", ca_key) #save ca_key in keys/ca_key.pem
     save_cert("keys/ca_cert.pem", ca_cert)
     save_cert("keys/sender_cert.pem", sender_cert)
 
